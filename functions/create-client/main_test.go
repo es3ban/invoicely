8dcd62e7fbc5f47cb7b9347b08fce04c351f98b2
es3ban <steve.nobles@smatrix.com>
2018-07-17 16:34:03 -0700

    Create main_test.go

---
+++ functions/create-client/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+	
+	"github.com/EwanValentine/invoicely/pkg/model"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/stretchr/testify/assert"
+)
+
+// A fake repository to DI into our handler
+type fakeRepo struct{}
+
+func (repo fakeRepo) Store(*model.Client) error {
+	return nil
+}
+
+func TestCanStoreClient(t *testing.T) {
+	request := events.APIGatewayProxyRequest{
+			Body: `{ "name": "test client", "description": "some test", "rate": 40 }`,
+		}
+	h := &handler{fakeRepo{}}
+	response, err := h.Handler(request)
+	assert.NoError(t, err)
+	assert.Equal(t, http.StatusCreated, response.StatusCode)
+}
